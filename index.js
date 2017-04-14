31664b98cda84a8276a4eaf2ba93713357304398
2D\shanx <shanx@slalom.com>
2017-04-14 14:25:48 -0700

    handle valid input package installation development + unit test

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-module.exports = {};
+module.exports = {
+    createDependencyOrder: function (inputArray){
+        // parse the original input into key value pair and return a new input
+        var parseInput = function(input) {
+            var newInput = {};
+            input.forEach(function(line) {
+                var pieces = line.split(':').map(function(element){
+                    return element.trim();
+                });
+                var package = pieces[0];
+                var dependencies = pieces[1].length ? pieces[1].split(',') : [];
+                newInput[package] = dependencies;
+            });
+            return newInput;
+        };
+
+        var parsedInput = parseInput(inputArray);
+        console.log('parsedInput', parsedInput);
+
+        // variable to keep track of handled dependencies output
+        var current_handled_dependencies = [];
+
+        // handle dependencies
+        var handleDependencies = function(package) {
+            // check to see if it's already handled
+            if( -1 == current_handled_dependencies.indexOf(package)) {
+                if (parsedInput[package] && parsedInput[package].length) {
+                    parsedInput[package].forEach(function(dependency){
+                        handleDependencies(dependency);
+                    });
+                }
+                current_handled_dependencies.push(package);
+            }
+
+        };
+
+        // loop through the parsed input and call handleDependencies function to handle dependencies
+        Object.keys(parsedInput).forEach(function(package){
+            handleDependencies(package);
+        });
+
+        console.log('current_handled_dependencies ', current_handled_dependencies);
+        // join the final output array to a comma separated string
+        return current_handled_dependencies.join(', ');
+    }
+};
--- test/test.js
+++ test/test.js
@@ -4,4 +4,32 @@ describe('package-installer', function(){
         var pkgInstaller = require('../index');
         assert.notEqual(undefined, pkgInstaller.createDependencyOrder);
     });
+    it('does the createDependencyOrder output a string', function(){
+        var pkgInstaller = require('../index');
+        var inputArray = [
+            "KittenService: ",
+            "Leetmeme: Cyberportal",
+            "Cyberportal: Ice",
+            "CamelCaser: KittenService",
+            "Fraudstream: Leetmeme",
+            "Ice: "
+        ];
+        assert.equal('string', typeof(pkgInstaller.createDependencyOrder(inputArray)));
+    });
+    describe('input output tests', function(){
+        it('valid input test', function(){
+            var pkgInstaller = require('../index');
+            var inputArray = [
+                "KittenService: ",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: Leetmeme",
+                "Ice: "
+            ];
+            var outputStr = pkgInstaller.createDependencyOrder(inputArray);
+            console.log('what is the output ', outputStr);
+            assert.equal(outputStr, 'KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream');
+        });
+    });
 });
